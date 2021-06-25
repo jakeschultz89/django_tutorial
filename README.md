ffb02ca03998b8464ea0c1b0444758aa8a40e8a0
jakeschultz89 <ejacobschultz@gmail.com>
2021-06-24 21:31:00 -0400

    working on API

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # django_tutorial
+
+https://docs.djangoproject.com/en/3.2/intro/tutorial01/
+
+https://docs.djangoproject.com/en/3.2/intro/tutorial02/
+
+https://docs.djangoproject.com/en/3.2/intro/tutorial03/
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,16 +4,11 @@ from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
-    # ...
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-# class Question(models.Model):
-#     # ...
-#     def __str__(self):
-#         return self.question_text
+    def __str__(self):
+        return self.question_text
 
 class Choice(models.Model):
-    # ...
     def __str__(self):
         return self.choice_text
